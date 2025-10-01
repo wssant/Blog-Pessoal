21ee0f2496a9091fc2f84343c9c980238d4dd5c8
Winnie <wssant13@gmail.com>
2025-10-01 08:11:05 -0300

    inserindo modulo auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { TemaModule } from './tema/tema.module';
     }),
     PostagemModule,
     TemaModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
