9142697a900d0cbf222f8de743419389938342eb
Winnie <wssant13@gmail.com>
2025-10-07 09:38:15 -0300

    deploy do projeto blog pessoal

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
