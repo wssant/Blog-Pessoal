da7a64d1ba3d1383470efdb8c2ea252c873e07fb
Winnie <wssant13@gmail.com>
2025-10-07 08:40:23 -0300

    Revert "dev e prod service"
    This reverts commit 1893966eb7a47fdf9892fede8537bc07f8159b90.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
